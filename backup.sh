aad17a4501e315b44b4e3c77d7add164e8df0ad3
bryan kennedy <bkennedy@smm.org>
2017-06-05 14:32:29 -0500

    Scripts to help us disable keys on an Ubuntu kiosk

---
+++ backup.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+gsettings list-recursively org.gnome.desktop.wm.keybindings |
+   perl -pe 's/(.*)\s*(\[.*?\])\s*$/$1\t$2\n/' |
+    while IFS=$'\t' read -r key val; do echo -e "$key\t$val"; done > old_settings
---
+++ disable.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+gsettings list-recursively org.gnome.desktop.wm.keybindings |
+    perl -pe 's/(.*)\s*(\[.*?\])\s*$/$1\t$2\n/' |
+        while IFS=$'\t' read -r key val; do gsettings set $key ['']; done
