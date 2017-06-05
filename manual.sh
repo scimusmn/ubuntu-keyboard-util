4344cb170e1d4faaa2acce834af42991db2e3faf
bryan kennedy <bkennedy@smm.org>
2017-06-05 15:08:20 -0500

    Adding a few more shortcuts we want to disable

--- manual.sh
+++ manual.sh
@@ -4,4 +4,8 @@ gsettings set org.gnome.settings-daemon.plugins.media-keys active false;
 gsettings set org.gnome.desktop.wm.keybindings activate-window-menu [''];
 gsettings set org.gnome.desktop.wm.keybindings switch-input-source [''];
 gsettings set org.gnome.desktop.wm.keybindings switch-input-source-backward [''];
+gsettings set org.gnome.desktop.wm.keybindings show-desktop [''];
+gsettings set org.gnome.desktop.wm.keybindings panel-run-dialog [''];
+gsettings set org.gnome.desktop.wm.keybindings cycle-windows [''];
+gsettings set org.gnome.desktop.wm.keybindings panel-main-menu [''];
 
